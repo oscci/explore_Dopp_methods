8fcb27d446af7caae6077e5bcd3bd6e8e371ccf4
Dorothy Bishop <dorothy.bishop@psy.ox.ac.uk>
2018-04-15 07:12:46 +0000

    Added more explanation

--- A2_outliercheck_se.R
+++ A2_outliercheck_se.R
@@ -23,20 +23,24 @@ myexclude <-data.frame(matrix(0,nrow=30,ncol=13))
   mysessdata2 <- read.csv(readfilename)
   se.cols <- seq(from=8, to = 73, by = 13)
   allsessdata <- cbind(mysessdata$Filename,mysessdata[,se.cols],mysessdata2[,se.cols])
-
-
-########################################################
-# Outliers are defined as the being 
-# more than 2.2 times the difference between the first and third quartiles (2.2 * (Q3-Q1)) 
-# below or above the first and third quartile values 
-# (e.g: lower limit = Q1 – 2.2*(Q3-Q1); upper limit = Q3 + 2.2*(Q3-Q1)). 
-# Because we are using SE, we only want to lose unusually high values, so focus on upper limit
-for (t in 1:12){
-  lower_quartile <- quantile(allsessdata[,t+1], probs=0.25, na.rm="TRUE")
-  upper_quartile <- quantile(allsessdata[,t+1], probs=0.75, na.rm="TRUE")
-  quartile_diff <- upper_quartile - lower_quartile
+  colnames(allsessdata)[1] <-'Filename'
+  ########################################################
+  # Outliers are defined as  being 
+  # more than 2.2 times the difference between the first and third quartiles (2.2 * (Q3-Q1)) 
+  # below or above the first and third quartile values 
+  # (e.g: lower limit = Q1 – 2.2*(Q3-Q1); upper limit = Q3 + 2.2*(Q3-Q1)). 
+  # Because we are using SE, we only want to lose unusually high values, so focus on upper limit
   
-  upper_limit <- upper_quartile + 2.2*quartile_diff
+ #Here base limits on all tasks, since we want same reliability cutoff for all
+  myv<-vector()
+  for (t in 1:12){
+    myv<-c(myv,allsessdata[,(t+1)])
+  }
+  lower_quartile <- quantile(myv,probs=.25)
+  upper_quartile <- quantile(myv,probs=.75)
+  quartile_diff <- upper_quartile - lower_quartile
+  upper_limit <- upper_quartile + 1.65*quartile_diff #one-tailed so use lower z-value
+ for (t in 1:12){
   w <- which(allsessdata[,t+1] > upper_limit)
   myexclude[w,t+1] <- 1
 }
